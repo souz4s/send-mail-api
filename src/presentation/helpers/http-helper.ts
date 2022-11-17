49a11d5d4693fff66ae3d4c8b0a5f108ac785954
souz4s <psouza.1650@gmail.com>
2022-11-16 23:22:39 -0300

    perf: removed http status parameter 201

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -2,19 +2,24 @@ import { InternalServerError } from "@/presentation/erros";
 import { HttpResponse } from "@/presentation/protocols";
 
 export class HttpHelper {
+  static CREATED = (): HttpResponse<void> => ({
+    statusCode: 201,
+    message: "Successfully created",
+  });
+
   static BAD_REQUEST = (err: Error): HttpResponse<Error> => ({
     statusCode: 400,
     body: err,
   });
 
+  static NOT_ACCEPTABLE = <T>(data: T): HttpResponse<T> => ({
+    statusCode: 406,
+    body: data,
+    message: "Sending rejected",
+  });
+
   static INTERNAL_SERVER_ERROR = (err: Error): HttpResponse<Error> => ({
     statusCode: 500,
     body: new InternalServerError(err.stack || ""),
-  });
-
-  static CREATED = <T>(data: T): HttpResponse<T> => ({
-    statusCode: 201,
-    body: data,
-    message: "Successfully created",
   });
 }

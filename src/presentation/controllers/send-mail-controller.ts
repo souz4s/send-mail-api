4b4213a18925dbc15d8e68f379dcb66734a8e91c
souz4s <psouza.1650@gmail.com>
2022-11-16 23:26:13 -0300

    feat: mail send controller

---
+++ src/presentation/controllers/send-mail-controller.ts
@@ -0,0 +1,26 @@
+import { AuthModel, MailModel } from "@/domain/models";
+import { SendMail } from "@/domain/use-cases";
+import { MissingParametersError } from "@/presentation/erros";
+import { HttpHelper } from "@/presentation/helpers";
+import { Controller } from "@/presentation/protocols";
+
+export class SendMailController implements Controller {
+  constructor(private readonly sendMail: SendMail) {}
+  handle = async (request: SendMailController.Request) => {
+    try {
+      const sendMail = request;
+      const requiredFields = sendMail.auth && sendMail.mailModel.from && sendMail.mailModel.to;
+      if (!requiredFields) return HttpHelper.BAD_REQUEST(new MissingParametersError());
+      if (sendMail.auth.user !== sendMail.mailModel.from)
+        return HttpHelper.NOT_ACCEPTABLE("The user's email cannot be different from the sender's email.");
+      await this.sendMail.perform(sendMail);
+      return HttpHelper.CREATED();
+    } catch (err) {
+      return HttpHelper.INTERNAL_SERVER_ERROR(err as Error);
+    }
+  };
+}
+
+export namespace SendMailController {
+  export type Request = { auth: AuthModel; mailModel: MailModel };
+}

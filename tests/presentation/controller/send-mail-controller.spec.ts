37084e0296af7fdd050a321f3118e43f894a8a20
souz4s <psouza.1650@gmail.com>
2022-11-17 22:20:39 -0300

    test: ensure return the status 406 when the user's email is different from the sender's email

--- tests/presentation/controller/send-mail-controller.spec.ts
+++ tests/presentation/controller/send-mail-controller.spec.ts
@@ -3,6 +3,8 @@ import { SendMailController } from "@/presentation/controllers";
 import { mockSendMailParams } from "@/tests/domain/remote";
 import { SendMailSpy } from "@/tests/presentation/mocks";
 
+import { faker } from "@faker-js/faker";
+
 const makeSut = () => {
   const sendMailSpy = new SendMailSpy();
   const sut = new SendMailController(sendMailSpy);
@@ -28,5 +30,15 @@ describe("SendMailController", () => {
     const result = await sut.handle({ auth: { pass: undefined }, mailModel: { from: undefined } });
     expect(sendMailSpy.callsCount).toBe(0);
     expect(result.statusCode).toBe(400);
+  });
+
+  it("should return the status not acceptable when the user's email is different from the sender's email", async () => {
+    const { sut, sendMailSpy } = makeSut();
+    const result = await sut.handle({
+      auth: { user: "api@test.com", pass: faker.internet.password() },
+      mailModel: { from: faker.internet.email(), to: faker.internet.email() },
+    });
+    expect(sendMailSpy.callsCount).toBe(0);
+    expect(result.statusCode).toBe(406);
   });
 });

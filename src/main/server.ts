78efa2637f731b9dd5d33cf6f45071e623548f23
souz4s <p.uzah@outlook.com>
2022-12-21 02:55:04 -0300

    refactor: server boot refactoring using best practices

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,12 @@
 import { App, Env } from "@/main/config";
 
-try {
-  const app = App();
-  app.listen(Env.PORT, () => console.log(`\n> Server is running on port ${Env.PORT}`));
-} catch (err) {
-  console.log(err);
-}
+const startServer = () => {
+  try {
+    const app = App();
+    app.listen(Env.PORT, () => console.log(`\n> Server is running on port ${Env.PORT}`));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+startServer();

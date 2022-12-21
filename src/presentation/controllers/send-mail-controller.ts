bc7b9fa50535996eae66dac7a3c1f68d28566ef4
souz4s <p.uzah@outlook.com>
2022-12-21 02:39:22 -0300

    refactor: reallocating try catch

--- src/presentation/controllers/send-mail-controller.ts
+++ src/presentation/controllers/send-mail-controller.ts
@@ -7,12 +7,12 @@ import { Controller } from "@/presentation/protocols";
 export class SendMailController implements Controller {
   constructor(private readonly sendMail: SendMail) {}
   handle = async (request: SendMailController.Request) => {
+    const sendMail = request;
+    const requiredFields = sendMail.auth && sendMail.mailModel.from && sendMail.mailModel.to;
+    if (!requiredFields) return HttpHelper.BAD_REQUEST(new MissingParametersError());
+    if (sendMail.auth.user !== sendMail.mailModel.from)
+      return HttpHelper.NOT_ACCEPTABLE("The user's email cannot be different from the sender's email.");
     try {
-      const sendMail = request;
-      const requiredFields = sendMail.auth && sendMail.mailModel.from && sendMail.mailModel.to;
-      if (!requiredFields) return HttpHelper.BAD_REQUEST(new MissingParametersError());
-      if (sendMail.auth.user !== sendMail.mailModel.from)
-        return HttpHelper.NOT_ACCEPTABLE("The user's email cannot be different from the sender's email.");
       await this.sendMail.perform(sendMail);
       return HttpHelper.CREATED();
     } catch (err) {

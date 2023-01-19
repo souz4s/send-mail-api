e1dcc64732b9f36388c97da14a0f582277e1fa26
souz4s <p.uzah@outlook.com>
2023-01-19 20:07:15 -0300

    refactor: make sendMail synchronous and return void

--- src/data/protocols/send-mail-adapter.ts
+++ src/data/protocols/send-mail-adapter.ts
@@ -1,7 +1,7 @@
 import { AuthModel, MailModel } from "../../domain/models";
 
 export interface SendMailAdapter {
-  sendMail: (params: SendMailAdapter.Params) => Promise<void>;
+  sendMail(params: SendMailAdapter.Params): void;
 }
 
 export declare namespace SendMailAdapter {

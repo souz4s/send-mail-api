a2d243235757f94276284028ef8310d989d1ace5
souz4s <p.uzah@outlook.com>
2023-01-21 04:01:32 -0300

    feat: email repository for sending email

---
+++ src/infrastructure/denomailer/repositories/mail-repository.ts
@@ -0,0 +1,24 @@
+import { SendMailAdapter } from "../../../data/protocols/index.ts";
+import { SMTPClient } from "../../denomailer/index.ts";
+
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+
+export class MailRepository implements SendMailAdapter {
+  constructor(private client: SMTPClient | undefined) {}
+  async sendMail(params: SendMailAdapter.Params): Promise<void> {
+    try {
+      const connectionOptions = {
+        connection: {
+          hostname: String(Deno.env.get("SMTP_HOSTNAME")),
+          port: Number(Deno.env.get("SMTP_PORT")),
+          tls: Boolean(Deno.env.get("SMTP_TLS")),
+          auth: params.auth,
+        },
+      };
+      this.client = new SMTPClient(connectionOptions);
+      await this.client.send(params.mailModel);
+    } catch (error) {
+      throw error;
+    }
+  }
+}

1ffe424f5320a17d08067ccc13064893749fef3e
souz4s <p.uzah@outlook.com>
2023-01-22 13:45:30 -0300

    refactor: removed unnecessary export and removed anonymous function

--- src/main/config/router.ts
+++ src/main/config/router.ts
@@ -3,6 +3,6 @@ import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { mailRoutes } from "../routes/index.ts";
 
 const router = new Router();
-router.use(() => mailRoutes);
+router.use(mailRoutes.routes());
 
-export { mailRoutes, router };
+export { router };

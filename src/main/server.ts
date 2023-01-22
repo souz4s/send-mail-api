63d6132b88c6236ce38dffddeae90e0ae22d624c
souz4s <p.uzah@outlook.com>
2023-01-22 17:25:36 -0300

    refactor: tweak in server startup message

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,18 +1,16 @@
 import { App, Env } from "./config/index.ts";
 
 const startServer = async () => {
-  let serverStarted = false;
   try {
     const app = await App();
-    app.listen({ port: Env.PORT });
-    serverStarted = true;
+    app.addEventListener(
+      "listen",
+      () => console.log(`\n> Server is running on port ${Env.PORT}`),
+    );
+    await app.listen({ port: Env.PORT });
   } catch (err) {
     console.error(err);
     Deno.exit(1);
-  }
-
-  if (serverStarted) {
-    console.log(`\n> Server is running on port ${Env.PORT}`);
   }
 };
 

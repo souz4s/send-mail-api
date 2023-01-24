f38de74c8e1453832f2c7bb6d1cdf9b1bb0d1b64
souz4s <p.uzah@outlook.com>
2023-01-24 01:10:26 -0300

    refactor: controller adapter adjustment according to response and request needed from sending e-mail

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,5 +1,19 @@
+import { AuthModel, MailModel } from "../../domain/models/index.ts";
 import type { HttpResponse } from "../protocols/index.ts";
 
-export interface Controller<T = unknown> {
-  handle: (params: T) => Promise<HttpResponse<T>>;
+export interface Controller {
+  handle: (
+    request: Controller.Request,
+  ) => Promise<HttpResponse<Controller.Response>>;
+}
+
+export declare namespace Controller {
+  export type Request = {
+    auth: AuthModel;
+    mailModel: MailModel;
+  };
+  export type Response = {
+    status: number;
+    body: string;
+  };
 }

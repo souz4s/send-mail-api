e003f57202c7beb6ee0d845f7f08507e76387457
souz4s <p.uzah@outlook.com>
2023-01-26 01:48:17 -0300

    fix: adjustment in passing the email sending parameters

--- src/infrastructure/denomailer/repositories/mail-repository.ts
+++ src/infrastructure/denomailer/repositories/mail-repository.ts
@@ -1,25 +1,20 @@
 import { SendMailAdapter } from "../../../data/protocols/index.ts";
-import { SMTPClient } from "../../denomailer/index.ts";
+import { denomailerClient } from "../../denomailer/index.ts";
 
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
 export class MailRepository implements SendMailAdapter {
-  private client: SMTPClient | undefined;
-  constructor() {}
-  async sendMail(params: SendMailAdapter.Params): Promise<void> {
-    try {
-      const connectionOptions = {
-        connection: {
-          hostname: String(Deno.env.get("SMTP_HOSTNAME")),
-          port: Number(Deno.env.get("SMTP_PORT")),
-          tls: Boolean(Deno.env.get("SMTP_TLS")),
-          auth: params.auth,
-        },
-      };
-      this.client = new SMTPClient(connectionOptions);
-      await this.client.send(params.mailModel);
-    } catch (error) {
-      console.log(error);
-    }
+  sendMail({ auth, mailModel }: SendMailAdapter.Params): void {
+    const { SMTP_HOSTNAME, SMTP_PORT } = Deno.env.toObject();
+    const connectionOptions = {
+      hostname: SMTP_HOSTNAME,
+      port: Number(SMTP_PORT),
+      ...auth,
+    };
+    denomailerClient.connect(connectionOptions)
+      .then(() => denomailerClient.send(mailModel))
+      .catch((error) => {
+        throw new Error(error);
+      });
   }
 }

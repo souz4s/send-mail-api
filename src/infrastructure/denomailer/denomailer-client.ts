f7d4bce2965905bac78d21aa84993cbe29882863
souz4s <p.uzah@outlook.com>
2023-01-27 01:56:25 -0300

    fix: removed import mapping due to incompatibility with deno deploy

--- src/infrastructure/denomailer/denomailer-client.ts
+++ src/infrastructure/denomailer/denomailer-client.ts
@@ -1,3 +1,3 @@
-import { SmtpClient } from "denomailer/mod.ts";
+import { SmtpClient } from "https://deno.land/x/denomailer@0.16.6/mod.ts";
 
 export const denomailerClient = new SmtpClient();
--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,7 @@
 import { router } from "./index.ts";
 
-import { oakCors } from "oakCors/oakCors.ts";
-import { Application } from "oak/application.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/oakCors.ts";
+import { Application } from "https://deno.land/x/oak@v11.1.0/application.ts";
 
 export const App = () => {
   const app = new Application();
